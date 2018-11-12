ea8c37e524d7285ee4e09f1647373f8722d6a424
sergeklokov <serge.klokov@gmail.com>
2018-11-11 19:49:31 -0700

    added config section

--- ConsoleMemcached/Program.cs
+++ ConsoleMemcached/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 using Enyim.Caching;
 using Enyim.Caching.Configuration;
 using Enyim.Caching.Memcached;
@@ -13,6 +15,11 @@ namespace ConsoleMemcached
     /// test of memcached with Enyim library
     /// http://downloads.northscale.com 
     /// memcached-win64-1.4.4-14.zip
+    /// 
+    /// Run in NuGet console:
+    /// Update-Package EnyimMemcached
+    ///
+    /// Update-Package �reinstall EnyimMemcached
     /// 
     /// run server: 
     /// memcached.exe --vv
@@ -25,32 +32,55 @@ namespace ConsoleMemcached
     {
         static void Main(string[] args)
         {
+            //load configuration at run time
+            //var config = new MemcachedClientConfiguration();
+            //config.AddServer("localhost:11211");
+            //config.AddServer("localhost:11212");  // let's add second server
+            //using (var memcachedClient = new MemcachedClient(config))
 
-            var config = new MemcachedClientConfiguration();
-            config.AddServer("localhost:11211");
-
-            using (var client = new MemcachedClient(config))
+            // get default configuration from app.config
+            // https://github.com/enyim/EnyimMemcached/wiki/MemcachedClient-Configuration
+            using (var memcachedClient = new MemcachedClient())
             {
+                memcachedClient.Store(Enyim.Caching.Memcached.StoreMode.Set, "checkVar", "blah-blah");
 
-                var currentTime = DateTime.Now.ToString();
-                Console.WriteLine("currentTime = " + currentTime);
+                var testValue = DateTime.Now.ToString();
+                Console.WriteLine("Set testValue = " + testValue);
 
                 // save to cache
-                client.Store(Enyim.Caching.Memcached.StoreMode.Set, "currentTime", currentTime);
+                var timeSpan = DateTime.Now.AddSeconds(4) - DateTime.Now;
+                memcachedClient.Store(Enyim.Caching.Memcached.StoreMode.Set, "testValue", testValue, timeSpan);
 
                 // retrieve
-                string value = client.Get<string>("currentTime");
-                Console.WriteLine("retrieved currentTime = " + value);
+                string value = memcachedClient.Get<string>("testValue");
+                Console.WriteLine("retrieved testValue = " + value);
+
+                Thread.Sleep(4000); // sleep 4 sec
+                value = memcachedClient.Get<string>("testValue");
+                Console.WriteLine("retrieved after 3 sec testValue = " + value);
 
             }
 
-
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key");
             Console.ReadKey();
-
-                // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
-            }
+        }
     }
+
+    // running two memcached servers at one machine:
+    //
+    // cd "C:\Program Files (x86)\memcached"
+    //
+    // with daemonize (-d)
+    // memcached -p 11211 -d   
+    // memcached -p 11212 -d
+    //
+    // memcached -p 11211 
+    // memcached -p 11212 
+    //
+    // memcached -p 11211 -vvv -m1
+    // memcached -p 11212 -vvv -m1
+    //
+    // telnet localhost 11211
+    // telnet localhost 11212
+
 }
